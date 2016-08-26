31532cad8db4d711eeaa7845997c793419b0c5da
Michael Brown <mike_brown@rogers.com>
2016-08-26 07:23:32 -0400

    Cursor Intro
    cursor does not change color
    change cursor to move to initial press location, then relocate to
    release location

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorController : MonoBehaviour {
+
+    public float floatDistance;
+    private float trueFloat;
+    private GameObject gameField;
+    private float startTime;
+    public Material idle;
+    public Material active;
+    private Renderer rend;
+
+    void Start () {
+        parentField();
+        floatParent();
+        //gameObject.SetActive(false);
+        rend = GetComponent<Renderer>();
+        rend.material = idle;
+        startTime = 0;
+	}
+
+    void Update ()
+    {
+        if (startTime != 0)
+        {
+            if (Time.realtimeSinceStartup - startTime == 0.25f)
+            {
+                rend.material = active;
+                transform.localScale = new Vector3(transform.localScale.x * 1.5f, transform.localScale.y * 1.5f, transform.localScale.z * 1.5f);
+            }
+            if (Time.realtimeSinceStartup - startTime == 0.75f)
+            {
+                rend.material = idle;
+                transform.localScale = new Vector3(transform.localScale.x * 0.66666f, transform.localScale.y * 0.66666f, transform.localScale.z * 0.66666f);
+            }
+            if (Time.realtimeSinceStartup - startTime == 1f)
+            {
+                startTime = 0;
+            }
+        }
+    }
+	
+	void parentField () {
+        Ray ray;
+        RaycastHit hit;
+        Vector3 position = transform.position;
+        Vector3 down = -transform.up;
+        ray = new Ray(position, down);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            gameField = hit.transform.gameObject;
+        }
+    }
+
+    void floatParent ()
+    {
+        Vector3 defaultFloat = new Vector3(gameField.transform.position.x, gameField.transform.position.y + (gameField.transform.localScale.y / 2) + floatDistance, gameField.transform.position.z);
+        transform.position = defaultFloat;
+    }
+
+    Vector3 translateToLocal (Vector3 original)
+    {
+        Vector3 local = new Vector3(original.x, transform.position.y, original.z);
+        return local;
+    }
+
+    public void pingPosition (Vector3 position)
+    {
+        Vector3 destination = translateToLocal(position);
+        transform.position = destination;
+        gameObject.SetActive(true);
+        startTime = Time.realtimeSinceStartup;
+    }
+}
--- Assets/Scripts/Viewfinder.cs
+++ Assets/Scripts/Viewfinder.cs
@@ -1,49 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Viewfinder : MonoBehaviour {
+public class ViewFinder : MonoBehaviour {
 
-    public GameObject playSubject;
-    public int moveSpeed;
-    private int layerMask = 1 << 8;
-    private Vector3 lookPoint;
+    private Vector3 gazePoint;
+    public CursorController cursor;
+    public string actionButton;
+    private bool buttonPressed;
 
-	// Use this for initialization
 	void Start () {
-	    if (moveSpeed == 0)
-        {
-            Debug.Log("User-defined speed was 0, defaulting to 100...");
-            moveSpeed = 100;
-        }
+        cursor = GameObject.Find("Cursor").GetComponent<CursorController>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
         Ray ray;
         RaycastHit hit;
         Vector3 cameraPosition = transform.position;
         Vector3 cameraForward = transform.forward;
         ray = new Ray(cameraPosition, cameraForward);
-
-        if (Input.GetButtonUp("Fire1"))
+        
+        if (Input.GetButtonDown(actionButton))
         {
-            if (Physics.Raycast(ray, out hit, layerMask))
-            {
-                lookPoint = hit.point;
-                movePlayerTo(lookPoint);
-            }
+            buttonPressed = true;
         }
-    }
-
-    void movePlayerTo (Vector3 position)
-    {
-        Vector3 oldPosition = playSubject.transform.position;
-        Vector3 moveDistance = new Vector3(lookPoint.x - playSubject.transform.position.x, lookPoint.y - playSubject.transform.position.y, lookPoint.z - playSubject.transform.position.z);
-
-        for (int i = 0; i == 100; i++)
+        if (Input.GetButtonUp(actionButton))
         {
-            float mult = i / 100;
-            playSubject.transform.position = oldPosition + new Vector3(moveDistance.x * mult,moveDistance.y * mult,moveDistance.z * mult);
+            buttonPressed = false;
+        }
+        if (buttonPressed == true)
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                gazePoint = hit.point;
+                cursor.pingPosition(gazePoint);
+            }
         }
     }
 }
